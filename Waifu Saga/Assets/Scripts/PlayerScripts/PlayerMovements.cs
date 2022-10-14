3d028c46e97e34366a0c5cf32a52c8e1bc8754b6
KaonWry <titan2011official@gmail.com>
2022-10-14 13:44:19 +0700

    Multiple jumps

--- Waifu Saga/Assets/Scripts/PlayerScripts/PlayerMovements.cs
+++ Waifu Saga/Assets/Scripts/PlayerScripts/PlayerMovements.cs
@@ -9,11 +9,12 @@ public class PlayerMovements : MonoBehaviour
     private Rigidbody2D body;
     private BoxCollider2D boxCollider;
     float horizontalMoveInput;
-
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask groundLayer;
     private bool isTouchingGround;
+    public int extraJumps;
+    private int jumpCounter;
 
     // Start is called before the first frame update
 
@@ -21,28 +22,46 @@ public class PlayerMovements : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
-
     }
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        // Horizontal movement
         horizontalMoveInput = Input.GetAxis("Horizontal");
-        
         if (horizontalMoveInput > 0.01f)
+        {
             transform.localScale = new Vector2(5, 5);
+        }
         else if (horizontalMoveInput < -0.01f)
+        {
             transform.localScale = new Vector2(-5, 5);
+        }
         
         body.velocity = new Vector2(horizontalMoveInput * speed, body.velocity.y);
 
-        if(Input.GetButtonDown("Jump") && isTouchingGround)
-            Jump();
+        //Jumping
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isTouchingGround) // Reset jump counter every time player lands
+        {
+            jumpCounter = extraJumps;
+        }
+
+        if(Input.GetButtonDown("Jump")) // Check jump input
+        {
+            if(isTouchingGround) // Check if player is on the ground
+            {
+                Jump();
+            }
+            else
+            {
+                if (jumpCounter > 0) // Check if jump counter above zero
+                {
+                    Jump();
+                    jumpCounter--;
+                }
+            }
+        }    
     }
 
     void Jump()
